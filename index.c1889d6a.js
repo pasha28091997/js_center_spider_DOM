f88e86620efe45acd14166aecfd64f16415ea6d3
Pasha Shchukin <pasha4320692@gmail.com>
2024-11-02 16:41:09 +0200

    make build

---
+++ index.c1889d6a.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider"),o=t.getBoundingClientRect(),i=e.getBoundingClientRect(),l=(o.width-i.width)/2,n=(o.height-i.height)/2;e.style.position="absolute",e.style.left="".concat(l,"px"),e.style.top="".concat(n,"px");
+//# sourceMappingURL=index.c1889d6a.js.map
